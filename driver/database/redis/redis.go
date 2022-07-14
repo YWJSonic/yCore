2d94967a9b4e0c8f6f247d5518419457c161db65
yang_yang <yang_yang@paradise-soft.com.tw>
2022-07-14 19:02:32 +0800

    [新增] redis driver

--- driver/database/redis/redis.go
+++ driver/database/redis/redis.go
@@ -1,10 +1,115 @@
 package redis
 
-// var Instance *RedisManager
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/url"
+	"strings"
+	"sync"
+	"time"
 
-func NewRedisManager() *RedisManager {
-	return &RedisManager{}
+	"github.com/go-redis/redis/v8"
+)
+
+var Instance *manager
+
+// New redis manager
+func New(addr, password string, poolSize int) error {
+
+	if Instance != nil {
+		return nil
+	}
+
+	ctx, cancel := context.WithCancel(context.TODO())
+	Instance = &manager{
+		redisClient: nil,
+		ctx:         ctx,
+	}
+
+	redisClient, err := Instance.init(addr, password, poolSize, ctx, cancel)
+	if err != nil {
+		fmt.Printf("[Redis][New] Init error: %v", err)
+		return err
+	}
+
+	Instance.redisClient = redisClient
+
+	fmt.Printf("[Redis][New] Connect success, address: %v", addr)
+	return nil
 }
 
-type RedisManager struct {
+type manager struct {
+	redisClient redis.Cmdable
+	ctx         context.Context
+	mu          sync.RWMutex
+}
+
+func (mgr *manager) init(addr, password string, poolSize int, ctx context.Context, cancel context.CancelFunc) (redis.Cmdable, error) {
+	redisIPs := strings.Split(addr, ",")
+	for _, ip := range redisIPs {
+		_, err := url.Parse("http://" + ip)
+		if err != nil {
+			fmt.Printf("[Redis][init] URL parse error: %v", err)
+			<-ctx.Done()
+			cancel()
+			return nil, err
+		}
+	}
+
+	client, err := mgr.connection(redisIPs, password, poolSize)
+	if err != nil {
+		fmt.Printf("[Redis][init] connection error, err: %v", err)
+		return nil, err
+	}
+
+	go pingLoop(ctx, client, cancel)
+
+	return client, nil
+}
+
+func (mgr *manager) connection(redisIPs []string, password string, poolSize int) (redis.Cmdable, error) {
+	switch len(redisIPs) {
+	case 0: // error
+		return nil, errors.New("[Redis][connection] IP address null")
+	case 1: // single
+		redisClient := redis.NewClient(&redis.Options{
+			Addr:       redisIPs[0],
+			Password:   password,
+			PoolSize:   poolSize,
+			MaxConnAge: 1 * time.Hour,
+		})
+		// redisClient.AddHook(apmgoredis.NewHook())
+
+		return redisClient, nil
+	default: // cluster
+		redisClient := redis.NewClusterClient(&redis.ClusterOptions{
+			Addrs:      redisIPs,
+			Password:   password,
+			PoolSize:   poolSize,
+			MaxConnAge: 1 * time.Hour,
+		})
+		// redisClient.AddHook(apmgoredis.NewHook())
+
+		return redisClient, nil
+
+	}
+}
+
+func pingLoop(ctx context.Context, redisClient redis.Cmdable, cancel context.CancelFunc) {
+	ticker := time.NewTicker(5 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			if _, err := redisClient.Ping(ctx).Result(); err != nil {
+				fmt.Printf("[Redis][pingLoop] ping error, err: %v", err.Error())
+				return
+			}
+		case <-ctx.Done():
+			cancel()
+			return
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -1,15 +1,32 @@
 module yangServer
 
-go 1.13
+go 1.18
 
 require (
 	github.com/chromedp/cdproto v0.0.0-20210429002609-5ec2b0624aec
 	github.com/chromedp/chromedp v0.7.1
+	github.com/go-redis/redis/v8 v8.11.5
 	github.com/gogo/protobuf v1.3.2
 	github.com/gorilla/websocket v1.4.2
 	go.uber.org/atomic v1.9.0
-	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110
-	golang.org/x/sys v0.0.0-20210503173754-0981d6026fa6 // indirect
+	golang.org/x/net v0.0.0-20210428140749-89ef3d95e781
 	google.golang.org/grpc v1.36.0
 	nhooyr.io/websocket v1.8.7
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/chromedp/sysutil v1.0.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.1.0-rc.5 // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/klauspost/compress v1.10.3 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/text v0.3.6 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
 )
