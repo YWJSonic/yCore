22cab06631fc43777323f428b0b66df5a062cada
yang <yangVM@gcp.com>
2022-07-22 09:31:12 +0000

    [修改] 調整 log

--- driver/database/nosql/arango/arango.go
+++ driver/database/nosql/arango/arango.go
@@ -16,7 +16,7 @@ func New(addr, username, password, database string) (*Manager, error) {
 		Endpoints: []string{addr},
 	})
 	if err != nil {
-		fmt.Printf("[Arango][New] Http new connection error, err: %v", err)
+		fmt.Printf("[Arango][New] Http new connection error, err: %v\n", err)
 		return nil, err
 	}
 
@@ -25,18 +25,18 @@ func New(addr, username, password, database string) (*Manager, error) {
 		Authentication: driver.BasicAuthentication(username, password),
 	})
 	if err != nil {
-		fmt.Printf("[Arango][New] Driver new client error, err: %v", err)
+		fmt.Printf("[Arango][New] Driver new client error, err: %v\n", err)
 		return nil, err
 	}
 
 	db, err := c.Database(context.TODO(), database)
 	if err != nil {
-		fmt.Printf("[Arango][New] Client database error, database: %v, err: %v", database, err)
+		fmt.Printf("[Arango][New] Client database error, database: %v, err: %v\n", database, err)
 		return nil, err
 	}
 
 	obj.Client = db
-	fmt.Printf("[Arango][New] Connect success, address: %v, database: %v", addr, database)
+	fmt.Printf("[Arango][New] Connect success, address: %v, database: %v\n", addr, database)
 
 	return obj, nil
 }
