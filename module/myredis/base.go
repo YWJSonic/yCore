d38a24c947328f6d1f93facc8bda22542b2bb57c
yang <yang@example.com>
2023-02-15 17:58:08 +0800

    modify redis init

--- module/myredis/base.go
+++ module/myredis/base.go
@@ -2,12 +2,15 @@ package myredis
 
 import (
 	"context"
+	"fmt"
+	"net/url"
+	"strings"
 	"ycore/driver/database/redis"
 	"ycore/module/mylog"
 )
 
 // New redis manager
-func New(addr, password string, poolSize int) (*Manager, error) {
+func New(addrT any, password string, poolSize int) (*Manager, error) {
 
 	ctx, cancel := context.WithCancel(context.TODO())
 	obj := &Manager{
@@ -15,15 +18,37 @@ func New(addr, password string, poolSize int) (*Manager, error) {
 		cancel: cancel,
 	}
 
-	driver, err := redis.New(addr, password, poolSize)
-	if err != nil {
-		mylog.Errorf("[Redis][init] connection error, err: %v", err)
-		return nil, err
+	switch iaddr := addrT.(type) {
+	case string:
+		redisIPs := strings.Split(iaddr, ",")
+		for _, ip := range redisIPs {
+			_, err := url.Parse("http://" + ip)
+			if err != nil {
+				mylog.Errorf("[Redis][New] url Parse error: %v", err)
+				return nil, err
+			}
+		}
+		driver, err := redis.New(redisIPs, password, poolSize)
+		if err != nil {
+			mylog.Errorf("[Redis][init] connection error, err: %v", err)
+			return nil, err
+		}
+
+		obj.Cmdable = driver
+	case []string:
+		driver, err := redis.New(iaddr, password, poolSize)
+		if err != nil {
+			mylog.Errorf("[Redis][init] connection error, err: %v", err)
+			return nil, err
+		}
+
+		obj.Cmdable = driver
+	default:
+		return nil, fmt.Errorf("[Redis][init] addr type error addr: %v", addrT)
 	}
 
-	obj.Cmdable = driver
-	go redis.PingLoop(ctx, cancel, driver)
+	go redis.PingLoop(ctx, cancel, obj.Cmdable)
 
-	mylog.Infof("[Redis][New] Connect success, address: %v", addr)
+	mylog.Infof("[Redis][New] Connect success, address: %v", addrT)
 	return obj, nil
 }
